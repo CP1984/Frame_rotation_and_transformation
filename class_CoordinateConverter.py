259cf749602545cfc46c14690ce6a880b6783df4
CP1984 <chingping1984@gmail.com>
2024-03-04 17:07:31 +0900

    add if __name__ == __main__:

--- class_CoordinateConverter.py
+++ class_CoordinateConverter.py
@@ -16,14 +16,15 @@ class CoordinateConverter:
         
         return r, theta_degrees, phi_degrees
 
-# Test
-x = 1
-y = 1
-z = 1
-# [-0.09497707  0.0434222   0.99453198]-0.09488667  0.04361939  0.99453198]
-converter = CoordinateConverter()
-r, theta, phi = converter.convert_cartesian_to_spherical(x, y, z)
-print(f"Cartesian coordinates ({x}, {y}, {z}) correspond to spherical coordinates:")
-print(f"Radius r = {r}")
-print(f"Polar angle θ = {theta} degrees")
-print(f"Azimuthal angle φ = {phi} degrees")
+if __name__ == "__main__":
+    # Test
+    x = 1
+    y = 1
+    z = 1
+    # [-0.09497707  0.0434222   0.99453198]-0.09488667  0.04361939  0.99453198]
+    converter = CoordinateConverter()
+    r, theta, phi = converter.convert_cartesian_to_spherical(x, y, z)
+    print(f"Cartesian coordinates ({x}, {y}, {z}) correspond to spherical coordinates:")
+    print(f"Radius r = {r}")
+    print(f"Polar angle θ = {theta} degrees")
+    print(f"Azimuthal angle φ = {phi} degrees")
